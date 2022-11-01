1b1223f4e1396461ca1e12853ebdb306f2085f8d
MacaDaca88 <114211123+MacaDaca88@users.noreply.github.com>
2022-11-01 16:28:30 +1300

    updated
    update of readme info
    update to code functions laser added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Robot
  robot
+
+
+squid game style robot with a cross of spin the bottle
+
+the robot will random spin and stop and differnt points when it stops at the point it will fire a laser
--- Sweep_learn.ino
+++ Sweep_learn.ino
@@ -8,6 +8,7 @@ Servo myservo;
 int led = 0;
 # define led A5
 # define ledred 3
+# define laser 4
 long randNumber;
 
 
@@ -18,29 +19,30 @@ void setup() {
   pinMode(led, OUTPUT);
   digitalWrite(led, LOW);
   pinMode(ledred, OUTPUT);
+    pinMode(laser, OUTPUT);
   digitalWrite(ledred, LOW);
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
 }
 
 void loop() {
 
-randNumber = random(180);  
+randNumber = random(180);  // set servo to random position upto 180deg
 
     
-    delay(5);
+    delay(5);         // wait time
       Serial.println(randNumber);
     if (randNumber > 140 ) {
       myservo.write(randNumber);
       digitalWrite(led, HIGH);
     }
-      delay(1000);
+      delay(1000);               // wait time
  
           myservo.write(0);
-    delay(5);
+    delay(5);         // wait time
     if ( randNumber < 55 ) {
        myservo.write(0);
       digitalWrite(led, LOW);
     }
  
-      delay(1000);
+      delay(1000);          // wait time
 }
