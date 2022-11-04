9a503a32a4b83dff06cef669d7120f81f2518800
MacaDaca88 <114211123+MacaDaca88@users.noreply.github.com>
2022-11-05 02:36:57 +1300

    Update Sweep_learn.ino

--- Sweep_learn.ino
+++ Sweep_learn.ino
@@ -3,14 +3,17 @@
 #include <Servo.h>
 
 
-Servo myservo;
+Servo myservofl;
+Servo myservofr;
+Servo myservorr;
+Servo myservorl;
 
 int led = 0;
 # define led A5
 # define ledred 3
 # define laser 4
 long randNumber;
-
+int pos = 0;    // variable to store the servo position
 
 void setup() {
 
@@ -21,28 +24,31 @@ void setup() {
   pinMode(ledred, OUTPUT);
     pinMode(laser, OUTPUT);
   digitalWrite(ledred, LOW);
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservofl.attach(5);  // attaches the servo on pin 9 to the servo object
+ myservofr.attach(6);
+ myservorr.attach(9);
+ myservorl.attach(10);
 }
 
 void loop() {
-
-randNumber = random(180);  // set servo to random position upto 180deg
-
-    
-    delay(5);         // wait time
-      Serial.println(randNumber);
-    if (randNumber > 140 ) {
-      myservo.write(randNumber);
-      digitalWrite(led, HIGH);
-    }
-      delay(1000);               // wait time
- 
-          myservo.write(0);
-    delay(5);         // wait time
-    if ( randNumber < 55 ) {
-       myservo.write(0);
-      digitalWrite(led, LOW);
-    }
- 
-      delay(1000);          // wait time
+for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservorr.write(pos);              // tell servo to go to position in variable 'pos'
+  myservorl.write(pos); 
+  myservofr.write(pos); 
+   myservofl.write(pos); 
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservorr.write(pos);              // tell servo to go to position in variable 'pos'
+  myservorl.write(pos); 
+  myservofr.write(pos); 
+   myservofl.write(pos); 
+    delay(15);                       // waits 15 ms for the servo to reach the position
+  }
+    myservorr.write(10);              // tell servo to go to position in variable 'pos'
+  myservorl.write(10); 
+  myservofr.write(10); 
+   myservofl.write(10);  
+   delay(5000);
 }
